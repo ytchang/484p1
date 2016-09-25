786c964af66ca58ac32f3497a55de398f7bff934
Ting-Jui Ho <gg8177818@yahoo.com.tw>
2016-09-25 17:38:12 -0400

    add loaddata and modified others13 add output csv

--- output_csv_file_script.sql
+++ output_csv_file_script.sql
@@ -3,8 +3,24 @@ set pagesize 0
 set trimspool on 
 set headsep off 
 set linesize 10000 
-spool userinfo.csv 
-SELECT * FROM PUBLIC_USER_INFORMATION; 
+
+spool PUBLIC_USER_INFORMATION.csv 
+SELECT * FROM keykholt.PUBLIC_USER_INFORMATION; 
 spool off
 
+spool PUBLIC_ARE_FRIENDS.csv 
+SELECT * FROM keykholt.PUBLIC_ARE_FRIENDS; 
+spool off
+
+spool PUBLIC_PHOTO_INFORMATION.csv 
+SELECT * FROM keykholt.PUBLIC_PHOTO_INFORMATION; 
+spool off
+
+spool PUBLIC_TAG_INFORMATION.csv 
+SELECT * FROM keykholt.PUBLIC_TAG_INFORMATION; 
+spool off
+
+spool PUBLIC_EVENT_INFORMATION.csv 
+SELECT * FROM keykholt.PUBLIC_EVENT_INFORMATION; 
+spool off
 
