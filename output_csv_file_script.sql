576480963ee01114302db77c5bcd7b741df67434
Ting-Jui Ho <gg8177818@yahoo.com.tw>
2016-09-24 22:08:56 -0400

    add spool file sql2

--- output_csv_file_script.sql
+++ output_csv_file_script.sql
@@ -1,10 +1,10 @@
-set colsep ',' --separatecolumns with a comma
-set pagesize 0 --No header rows
-set trimspool on --removetrailing blanks
-set headsep off --this may or may not be useful ... depends on your headings .
-set linesize 10000 --linesize should be > sum of the column widths
-spool userinfo.csv --output will be spooled to this file
-SELECT * FROM PUBLIC_USER_INFORMATION; --type your query here
+set colsep ',' 
+set pagesize 0 
+set trimspool on 
+set headsep off 
+set linesize 10000 
+spool userinfo.csv 
+SELECT * FROM PUBLIC_USER_INFORMATION; 
 spool off
 
 
