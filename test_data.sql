38c0efa725c0657dfc8cabc796c1584d36bad331
Ting-Jui Ho <gg8177818@yahoo.com.tw>
2016-09-25 21:59:45 -0400

    add loaddata and modified others13 add output csv2

---
+++ test_data.sql
@@ -0,0 +1,50 @@
+INSERT INTO USERS
+(USER_ID, FIRST_NAME, LAST_NAME, YEAR_OF_BIRTH, MONTH_OF_BIRTH, DAY_OF_BIRTH, GENDER) 
+VALUES
+('1','Ray1','Ho1','1','1','1','1','male');
+
+INSERT INTO USERS
+(USER_ID, FIRST_NAME, LAST_NAME, YEAR_OF_BIRTH, MONTH_OF_BIRTH, DAY_OF_BIRTH, GENDER) 
+VALUES
+('2','Ray2','Ho2','2','2','2','2','female');
+
+INSERT INTO USERS
+(USER_ID, FIRST_NAME, LAST_NAME, YEAR_OF_BIRTH, MONTH_OF_BIRTH, DAY_OF_BIRTH, GENDER) 
+VALUES
+('3','Ray3','Ho3','3','3','3','3','male');
+
+INSERT INTO USERS
+(USER_ID, FIRST_NAME, LAST_NAME, YEAR_OF_BIRTH, MONTH_OF_BIRTH, DAY_OF_BIRTH, GENDER) 
+VALUES
+('4','Ray4','Ho4','4','4','4','4','female');
+------------------------------------------------------------------------------------
+INSERT INTO FRIENDS 
+(USER1_ID, USER2_ID)
+VALUES
+('1','2');
+
+INSERT INTO FRIENDS 
+(USER1_ID, USER2_ID)
+VALUES
+('2','1');
+
+INSERT INTO FRIENDS 
+(USER1_ID, USER2_ID)
+VALUES
+('1','3');
+
+INSERT INTO FRIENDS 
+(USER1_ID, USER2_ID)
+VALUES
+('4','3');
+
+INSERT INTO FRIENDS 
+(USER1_ID, USER2_ID)
+VALUES
+('3','4');
+------------------------------------------------------------------------------------
+
+
+
+
+
